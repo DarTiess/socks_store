325727b0ed38e920d945272e92a2d426c9dd4e5b
DariyaTiyessova <bermuds@inbox.ru>
2026-03-19 13:09:06 +0500

    init

---
+++ src/main/java/org/example/socks_store/SocksStoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.socks_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocksStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/socks_store/SocksStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.socks_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
